20593157970ddc918a2e39ca3671b00fcaa2e7cb
Glitch (id-pets) <none>
2021-04-18 23:39:26 +0000

    🍙🌓 Updated 'botao cadastro'

--- script.js
+++ script.js
@@ -9,3 +9,89 @@ setInterval(function() {
     }
 }, 5000);
 
+// Pontuações no CPF preenchem automaticamente.
+function mascara(i){
+   
+    var v = i.value;
+    
+    if(isNaN(v[v.length-1])){ // impede entrar outro caractere que não seja número
+       i.value = v.substring(0, v.length-1);
+       return;
+    }
+    
+    i.setAttribute("maxlength", "14");
+    if (v.length == 3 || v.length == 7) i.value += ".";
+    if (v.length == 11) i.value += "-";
+ 
+ }
+
+ // Pontuações de DDD e traço do TELEFONE preenchem automaticamente
+ function mask(o, f) {
+  setTimeout(function() {
+    var v = mphone(o.value);
+    if (v != o.value) {
+      o.value = v;
+    }
+  }, 1);
+}
+
+function mphone(v) {
+  var r = v.replace(/\D/g, "");
+  r = r.replace(/^0/, "");
+  if (r.length > 10) {
+    r = r.replace(/^(\d\d)(\d{5})(\d{4}).*/, "($1) $2-$3");
+  } else if (r.length > 5) {
+    r = r.replace(/^(\d\d)(\d{4})(\d{0,4}).*/, "($1) $2-$3");
+  } else if (r.length > 2) {
+    r = r.replace(/^(\d\d)(\d{0,5})/, "($1) $2");
+  } else {
+    r = r.replace(/^(\d*)/, "($1");
+  }
+  return r;
+}
+
+// Validar senha - Verifica se a senha e o confirmar senha está correto.
+let senha = document.getElementById('senha');
+let senha2 = document.getElementById('senha2');
+
+function validarSenha() {
+  if (senha.value != senha2.value) {
+    senha2.setCustomValidity("Senhas diferentes!");
+    senha2.reportValidity();
+    return false;
+  } else {
+    senha2.setCustomValidity("");
+    return true;
+  }
+}
+
+// Quando o formulario está preenchido o botão "cadastrar" se habilita.
+function checkInputs(inputs) {
+   var filled = true;
+   
+   inputs.forEach(function(input) {
+       
+     if(input.value === "") {
+         filled = false;
+     }
+   
+   });
+   
+   return filled;
+   
+ }
+ var inputs = document.querySelectorAll("input");
+ var button = document.querySelector("button");
+ inputs.forEach(function(input) {
+     
+   input.addEventListener("keyup", function() {
+     if(checkInputs(inputs)) {
+       button.disabled = false;
+       document.getElementById('Cadastrar').style.display = 'block';
+
+     } else {
+       button.disabled = true;
+       document.getElementById('Cadastrar').style.display = 'none';
+     }
+   });
+ });
